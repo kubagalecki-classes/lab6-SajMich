8d4738b3af1899210826d7b383e82ee592967b51
SajMich <michal.sajewicz.stud@pw.edu.pl>
2020-12-14 12:09:04 +0000

    solved1

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,79 @@
+#include "make_random_vector.hpp"
 #include <iostream>
+#include <list>
+#include <vector>
+
+int glob;
+
+struct comp
+{
+    template < typename T >
+    bool operator()(const T& l, const T& r) const
+    {
+        return l > r;
+    }
+};
+
+struct bigger7
+{
+    template < typename T >
+    bool operator()(const T& a) const
+    {
+        return a >= 7;
+    }
+};
+
+struct K
+{
+    K(int tmp) { k = tmp; }
+    int k;
+    template < typename T >
+    bool operator()(const T& a) const
+    {
+        return a >= k;
+    }
+};
+
+struct test
+{
+    test() { a = 10; }
+    test(int tmp) { a = tmp; }
+    int  a;
+    void print() { std::cout << "Print! " << a << "\n"; }
+    bool check()
+    {
+        if (a == 10) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    auto                print = [](char& c) { std::cout << " " << c << "\n"; };
+    auto                fill  = [](char& c) { c = 'f'; };
+    std::vector< char > ret_v(10);
+    std::for_each(ret_v.begin(), ret_v.end(), fill);
+    std::for_each(ret_v.begin(), ret_v.end(), print);
+
+    std::list< test > T;
+    T.push_back(test(11));
+    T.push_back(test());
+    auto sprawdzenie = [](test& tmp) {
+        if (tmp.check()) {
+            return 'y';
+        }
+        else {
+            return 'n';
+        }
+    };
+
+    std::for_each(T.begin(), T.end(), [](test& tmp) { tmp.print(); });
+    std::transform(T.begin(), T.end(), ret_v.rbegin(), sprawdzenie);
+    std::for_each(ret_v.begin(), ret_v.end(), print);
+
+    // std::for_each(ret_v.begin(), ret_v.end(), [](char& c) { std::cout << c << "\n"; });
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,7 +10,18 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(),[](Human& h){h.birthday();} );
+
+  auto sprawdzenie = [](Human& tmp) {
+        if (tmp.isMonster()) {
+            return 'n';
+        }
+        else {
+            return 'y';
+        }
+    };
+
+    std::transform(people.begin(),people.end(), ret_v.rbegin(),sprawdzenie);
 
     return ret_v;
 }
